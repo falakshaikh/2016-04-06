78bdd9c86f511972f9802a980a0b23e4230ceaad
Falak Shaikh <falakshaikh1998@gmail.com>
2016-04-06 21:23:30 -0400

    buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(document).ready( function(){
-  alert("STILL WORKING")
+$(document).ready( function() {
+
+  $("#meow").click( function() {
+    $("#animals").append("<img class='animal' src='images/cat.png' alt='He Peek'/> ");
+  });
+
+  $("#bunbun").click( function() {
+    $("#animals").append("<img class='animal' src='images/bunny.png' alt='He Hop'/> ");
+  });
+
+  $(".animal").click( function() {
+    this.css('visibility', 'hidden')
+  });
+
 });
